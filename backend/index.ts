90187c1c9f2f145d61e574c8a450cf316575ceba
emingbt <mehmet.e.gbt@gmail.com>
2025-04-09 12:02:54 +0300

    Update backend serve port to 8080

--- backend/index.ts
+++ backend/index.ts
@@ -22,6 +22,6 @@ app.get("/test", (req, res) => {
   sendGcodeCommands("test")
 })
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+app.listen(8080, () => {
+  console.log("Server is running on port 8080")
 })
