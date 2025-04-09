6e6bebee29814a5bc564b2263861c25a701e583c
emingbt <mehmet.e.gbt@gmail.com>
2025-04-09 11:40:51 +0300

    Update tailwind

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image"
-import DrawingPage from "./components/DrawingCanvas"
+import DrawingCanvas from "../components/DrawingCanvas"
 
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <DrawingPage />
+      <DrawingCanvas />
     </div>
   )
 }
