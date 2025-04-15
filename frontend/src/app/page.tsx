e88df12017b07530bfc23eb6d6ac37b78adcc745
emingbt <mehmet.e.gbt@gmail.com>
2025-04-15 14:43:53 +0300

    Update style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import DrawingCanvas from "../components/DrawingCanvas"
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="w-full min-h-[calc(100vh-72px)] flex justify-center items-center bg-gray-100 py-4">
       <DrawingCanvas />
     </div>
   )
